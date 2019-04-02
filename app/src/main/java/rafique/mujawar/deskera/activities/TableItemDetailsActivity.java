3ed648367a0e0b8aa36bdc6eb98d39ace97916e7
Rafique <rafique.mujawar@tcognition.com>
2019-04-02 20:30:52 +0530

    temp commit

--- app/src/main/java/rafique/mujawar/deskera/activities/TableItemDetailsActivity.java
+++ app/src/main/java/rafique/mujawar/deskera/activities/TableItemDetailsActivity.java
@@ -2,10 +2,15 @@ package rafique.mujawar.deskera.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import rafique.mujawar.deskera.R;
 
 public class TableItemDetailsActivity extends AppCompatActivity {
+
+  private EditText metItem;
+  private TextView mtvPrimary,mtvTitle, mtvSecondary;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
